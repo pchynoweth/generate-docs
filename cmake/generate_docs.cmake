703c8262d11a0ae7ea5401ab6006a7f079614cba
pchynoweth <24738364+pchynoweth@users.noreply.github.com>
2020-07-04 16:12:04 +0100

    Tweaks

--- cmake/generate_docs.cmake
+++ cmake/generate_docs.cmake
@@ -1,12 +1,20 @@
 file(GLOB TRANSFORM_SCRIPT ./scripts/transform.py)
 
-function(generate_docs TARGET)
-    cmake_parse_arguments(GEN "" "" "SOURCES;PYTHON_SOURCES" ${ARGN})
+function(generate_docs PROJECT)
+    cmake_parse_arguments(GEN "" "" "SOURCES" ${ARGN})
 
-    foreach(FILE_PATH ${GEN_PYTHON_SOURCES})
+    set(PYTHON_SOURCES ${GEN_SOURCES})
+    list(FILTER PYTHON_SOURCES INCLUDE REGEX "\.py$")
+    list(FILTER GEN_SOURCES EXCLUDE REGEX "\.py$")
+
+    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/docs/)
+
+    foreach(FILE_PATH ${PYTHON_SOURCES})
         get_filename_component(PYTHON_FILE ${FILE_PATH} NAME)
 
+        # remove .py extension
         string(REGEX REPLACE "\\.[^.]*$" "" OUTPUT_FILE ${PYTHON_FILE})
+
         string(PREPEND OUTPUT_FILE ${PROJECT_BINARY_DIR}/docs/)
 
         list(APPEND OUTPUT_FILES ${OUTPUT_FILE})
@@ -16,9 +24,16 @@ function(generate_docs TARGET)
             DEPENDS ${FILE_PATH} ${PROJECT_BINARY_DIR}/docs/)
     endforeach()
 
-    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/docs/)
+    # non-python sources
+    foreach(FILE_PATH ${GEN_SOURCES})
+        get_filename_component(OUTPUT_FILE ${FILE_PATH} NAME)
+        string(PREPEND OUTPUT_FILE ${PROJECT_BINARY_DIR}/docs/)
 
-    add_custom_target(${TARGET}
+        list(APPEND OUTPUT_FILES ${OUTPUT_FILE})
+        configure_file(${FILE_PATH} ${OUTPUT_FILE} COPYONLY)
+    endforeach()
+
+    add_custom_target(${PROJECT}_generate_docs
         ALL
         DEPENDS ${OUTPUT_FILES})
 endfunction()
